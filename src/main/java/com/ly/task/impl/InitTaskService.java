6931af9821b85c3600337fd0ac47b5132182f5c2
lizhnekui <lizhenkui@qian88.com.cn>
2019-10-19 14:11:43 +0800

    提交

--- src/main/java/com/ly/task/impl/InitTaskService.java
+++ src/main/java/com/ly/task/impl/InitTaskService.java
@@ -18,6 +18,7 @@ public class InitTaskService implements ITaskService {
 
 	@Autowired
 	private RedisUtil redisUtil;
+	@Autowired
 	PageUtil pageUtil;
 	static Map<String, String> cookieMap = new HashMap<>();
 
@@ -55,7 +56,7 @@ public class InitTaskService implements ITaskService {
 		System.out.println("initTask =" + url);
 
 		try {
-			savePageUrl(url,nextPage, totalPage);
+			pageUtil.savePageUrl(url,nextPage, totalPage);
 
 		} catch (Exception e) {
 			redisUtil.rPush(redisKey, dealInfo);
@@ -63,43 +64,4 @@ public class InitTaskService implements ITaskService {
 			return;
 		}
 	}
-
-	private boolean savePageUrl(String dealUrl,String nextUrl, int totalPage) throws Exception {
-		boolean result = false;
-
-		if(StringUtils.isEmpty(nextUrl)){
-			redisUtil.lSet("firstPageList", dealUrl);
-
-			for (int i = 1; i < totalPage; i++) {
-
-				Map<String,String> map = UrlUtil.getQuestParam(dealUrl);
-				map.put("page" ,(i + 1) + "");
-				map.put("ref","sr_pg_" + (i + 1));
-				String tempUrl = UrlUtil.assembleUrl(map.get("host"),map);
-				redisUtil.lSet("firstPageList", tempUrl);
-			}
-			
-			result = true;
-
-		}else if (!StringUtils.isEmpty(nextUrl)) {
-			
-			redisUtil.lSet("firstPageList", dealUrl);
-			
-			redisUtil.lSet("firstPageList", nextUrl);
-
-			for (int i = 2; i < totalPage; i++) {
-
-				if (null != nextUrl && !nextUrl.equals("")) {
-					nextUrl = nextUrl.replace(i + "", (i + 1) + "");
-				}
-
-				redisUtil.lSet("firstPageList", nextUrl);
-			}
-			
-			result = true;
-		}
-
-		return result;
-	}
-	
 }
